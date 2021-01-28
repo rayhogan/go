e1b7df86fc89f4d8efafe63ce10f5fc9c918ae73
Ray Hogan <rayhogan@gmail.com>
2021-01-28 21:26:06 +0000

    Multi greeting

--- hello/hello.go
+++ hello/hello.go
@@ -11,12 +11,15 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
     // Get a greeting message and print it.
-    message, err := greetings.Hello("Ray")
+    messages, err := greetings.Hellos(names)
 
     if err != nil {
         log.Fatal(err)
     }
 
-    fmt.Println(message)
+    fmt.Println(messages)
 }
